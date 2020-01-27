508631a4103cbde02658dd227d4827cf7b764c17
acbarker19 <acbarker19@gmail.com>
2020-01-27 11:55:11 -0500

    Added Class Structure
    Added the required classes for Project 1: and uninformed search.

---
+++ src/AI_Basic/Problem.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI_Basic;
+
+/**
+ *
+ * @author acbar
+ */
+public class Problem {
+    // M&C / 8puzzle subclasses
+    // constructor parameters are start state and end state
+    /* Use 4 points from pg 66-67:
+    initialState(state),
+    actions(state) -> returns array of possible actions,
+    result(state, action) -> returns a state after performing an action,
+    goalTest(state) -> returns boolean if it reaches goal
+    */
+    
+    private State initialState, currentState, endState;
+    
+    public Problem(){
+        
+    }
+    
+    public Problem(State initialState, State endState){
+        this.initialState = initialState;
+        this.endState = endState;
+    }
+    
+    public State[] getActions(State currentState){
+        State[] possibleStates = new State[0];
+        
+        
+        return possibleStates;
+    }
+    
+    public State getResult(State currentState, Action actionTaken){
+        State nextState = new State("TEST");
+        
+        return nextState;
+    }
+     
+    public boolean goalTest(State testState){
+        if(testState == endState){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/AI_Basic/State.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI_Basic;
+
+/**
+ *
+ * @author acbar
+ */
+public class State {
+    
+    private String data;
+    
+    public State(String data){
+        this.data = data;
+    }
+    
+    public String toString(){
+        return data;
+    }
+    
+}
