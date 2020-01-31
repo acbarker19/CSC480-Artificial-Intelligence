8dcc140166b3aeef872d65ffbef049a1d12cc3a8
acbarker19 <acbarker19@gmail.com>
2020-01-31 11:20:43 -0500

    Added Node and Sequence
    Added code to the Node class for keeping track of what steps have been taken and the Sequence class to keep a list of actions to get to the current Node.

--- src/AI_Basic/Node.java
+++ src/AI_Basic/Node.java
@@ -11,4 +11,83 @@ package AI_Basic;
  */
 public class Node {
     
+    private State state;        // The state to which the node corresponds
+    private Node parentNode;    // The node in the search tree that generated this node
+    private Action action;      // The action that was applied to the parent to generate this node
+    private int pathCost;       // The cost - g(n) - of the path from the initial state to the
+                                // node using the parent pointers
+    
+    public Node(State state,
+                 Node parentNode,
+                 Action action,
+                 int pathCost){
+        this.state = state;
+        this.parentNode = parentNode;
+        this.action = action;
+        this.pathCost = pathCost;
+    }
+    
+    public void setState(State state){
+        this.state = state;
+    }
+    
+    public void setParentNode(Node parentNode){
+        this.parentNode = parentNode;
+    }
+    
+    public void setAction(Action action){
+        this.action = action;
+    }
+    
+    public void setPathCost(int pathCost){
+        this.pathCost = pathCost;
+    }
+    
+    public State getState(){
+        return state;
+    }
+    
+    public Node getParentNode(){
+        return parentNode;
+    }
+    
+    public Action getAction(){
+        return action;
+    }
+    
+    public int getPathCost(){
+        return pathCost;
+    }
+    
+    public String toString(){
+        return "State: " + state + ", Parent Node: " + parentNode +
+                ", Action: " + action + ", Path Cost: " + pathCost;
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Test main for Node class");
+        
+        State state = new State("33100");
+        Action action = new Action("row#11");
+        Node theNode = new Node(state,
+                                null,
+                                action,
+                                1);             // path cost
+        
+        System.out.println("theNode.toString(): " + theNode.toString());
+        Sequence theSequence = new Sequence(theNode.getAction());
+        System.out.println("theSequence.toString(): " + theSequence.toString());
+        
+        State stateOfChild = new State("220111");
+        Action actionOfChild = new Action("row#10");
+        Node theChildNode = new Node(stateOfChild,
+                                 theNode,       // parent node
+                                 actionOfChild,
+                                 1);            // path cost
+        
+        System.out.println("theChildNode.toString(): " + theChildNode.toString());
+        theSequence.add(theChildNode.getAction());
+        System.out.println("theSequence.toString(): " + theSequence.toString());
+    }
+    
 }
--- src/AI_Basic/Sequence.java
+++ src/AI_Basic/Sequence.java
@@ -5,10 +5,104 @@
  */
 package AI_Basic;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author acbar
  */
 public class Sequence {
+    private ArrayList<Action> actions;
+    
+    public Sequence(){
+        actions = new ArrayList<Action>();
+    }
+    
+    public Sequence(Action action){
+        actions = new ArrayList<Action>();
+        actions.add(action);
+    }
+    
+    public Sequence(ArrayList<Action> actions){
+        this.actions = actions;
+    }
+    
+    public boolean isEmpty(){
+        if(actions.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void add(Action action){
+        actions.add(action);
+    }
+    
+    public Action get(int identifier){
+        try{
+            return actions.get(identifier);
+        }catch(IndexOutOfBoundsException ioobe){
+            return null;
+        }
+    }
+    
+    public ArrayList<Action> getList(){
+        return actions;
+    }
+    
+    public int size(){
+        return actions.size();
+    }
+    
+    public String toString(){
+        String sequence = null;
+        
+        if(actions.isEmpty()){
+            sequence = "The sequence is empty.";
+        }else{
+            sequence = "";
+            for(int i = 0; i < actions.size(); i++){
+                sequence += actions.get(i).toString();
+                
+                if(i != actions.size() - 1){
+                    sequence += " ";
+                }
+            }
+        }
+        
+        return sequence;
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Test main for Sequence class");
+        
+        Sequence sequence = new Sequence();
+        Action action = new Action("row#11");
+        
+        System.out.println("Sequence is empty: " + sequence.isEmpty());
+        System.out.println("Sequence in order: " + sequence.toString());
+        
+        sequence.add(action);
+        System.out.println("Sequence is empty: " + sequence.isEmpty());
+        System.out.println("Sequence #10: " + sequence.get(10));
+        System.out.println("Sequence #0: " + sequence.get(0));
+        
+        Action action2 = new Action("row#10");
+        sequence.add(action2);
+        System.out.println("Sequence in order: " + sequence.toString());
+        
+        ArrayList<Action> actions = new ArrayList<Action>();
+        actions.add(new Action("row#01"));
+        actions.add(new Action("row#10"));
+        actions.add(new Action("row#11"));
+        actions.add(new Action("row#20"));
+        actions.add(new Action("row#02"));
+        
+        Sequence sequence2 = new Sequence(actions);
+        System.out.println("Sequence2 in order: " + sequence2.toString());
+        System.out.println("Sequence2 #3: " + sequence2.get(3));
+        
+    }
     
 }
